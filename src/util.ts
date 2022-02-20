660fc1f2d31ca1c78280d080a924a7a81e8e43e2
Levy Barbosa <marcio.levy20@gmail.com>
2022-02-20 18:50:12 -0300

    fix in private properties

--- src/util.ts
+++ src/util.ts
@@ -3,6 +3,7 @@ export function getPublicState(obj: any): any {
   return new Proxy(obj, {
     get: (target, key: string) => {
       if(key.startsWith('_')) return undefined;
+      if(typeof target[key] === 'object') return getPublicState(target[key]);
       return target[key];
     }
   });
